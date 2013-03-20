6ee94aab75cb74e9de7cf55b9797e213bd0c9a4e
Szabolcs Hubai <szab.hu@gmail.com>
2013-03-21 00:39:26 +0100

    Issue #49 - Add bayimg provider

--- extension/chrome/content/screenshot/providers.js
+++ extension/chrome/content/screenshot/providers.js
@@ -56,4 +56,40 @@ var PostimageDotOrg = {
   }
 }
 
+var bayimg = {
+  genRemovalCode: function (aLength)
+  {
+    var len = aLength || 10;
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i=0; i < len; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+  },
+
+  addFormFields: function(formdata)
+  {
+    formdata.addControl("code", bayimg.genRemovalCode());
+  },
+  
+  getFileFormField: function()
+  {
+    return "file";
+  },
+  
+  getReferer: function()
+  {
+    return "http://bayimg.com";
+  },
+  
+  getSubmissionURL: function()
+  {
+    return "http://upload.bayimg.com/upload";
+  }
+}
+
 Providers.addProvider(PostimageDotOrg);
+Providers.addProvider(bayimg);
