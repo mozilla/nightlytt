eb98a8e68fea6d92ffc845444bdb432380938cf9
Henrik Skupin <mail@hskupin.info>
2016-10-12 16:34:35 +0200

    Re-add import of AppConstants.jsm for SeaMonkey because it's not available by default

--- extension/chrome/content/suite.js
+++ extension/chrome/content/suite.js
@@ -97,6 +97,7 @@ getWindowTitleForNavigator: function () {
   var sep = docElement.getAttribute("titlemenuseparator");
   var modifier = "";
 
+  Components.utils.import("resource://gre/modules/AppConstants.jsm");
   var platformIsMac = AppConstants.platform == "macosx";
 
   if (!platformIsMac) {
